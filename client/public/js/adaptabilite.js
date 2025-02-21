b764c4c9b48086818a581eda685d57d6a67dbe82
olidesmarais <71419150+olidesmarais@users.noreply.github.com>
2025-02-21 16:20:55 -0500

    Bug reload sur mobile : Retrait du eventListener de resize
    Je ne suis pas certain de du rôle de la fonction, mais je ne vois pas d'impact majeur à son retrait. Le styling est évidemment à revoir.

--- client/public/js/adaptabilite.js
+++ client/public/js/adaptabilite.js
@@ -9,21 +9,6 @@ let set_police = () => {
     document.documentElement.style.setProperty(width, largeur + 'px');
 };
 
-let resizeTimer;
-window.addEventListener('resize', () => {
-    console.log('RESIZE');
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(
-        finResize = (() => {
-            set_police();
-            construire_elements();
-            afficherElements();
-        })(),
-        1000
-    );
-    
-});
-
 document.addEventListener("DOMContentLoaded", function(){
     isTouchDevice = 'ontouchstart' in document.documentElement;
     set_police();
